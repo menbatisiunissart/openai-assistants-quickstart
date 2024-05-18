37159b518855da0453263d3333ff5d397f76a00d
menbatisiunissart <aboulenger@gmail.com>
2024-05-18 22:50:31 +0100

    added blyng logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ export const metadata = {
   title: "Assistants API Quickstart",
   description: "A quickstart template using the Assistants API with OpenAI",
   icons: {
-    icon: "/openai.svg",
+    // icon: "/openai.svg",
+    icon: "/blyng.svg",
   },
 };
 
@@ -17,7 +18,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {assistantId ? children : <Warnings />}
-        <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
+        {/* <img className="logo" src="/openai.svg" alt="OpenAI Logo" /> */}
+        <img className="logo" src="/blyng.svg" alt="blyng Logo" />
       </body>
     </html>
   );
